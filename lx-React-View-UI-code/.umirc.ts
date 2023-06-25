ee969cc9ff593cbfc6bb6a8841313b30fce045bb
username_Lx <2308483480@qq.com>
2023-06-25 21:56:22 +0800

    feat(src/Button): 新增Button按钮功能

--- lx-React-View-UI-code/.umirc.ts
+++ lx-React-View-UI-code/.umirc.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  title: 'React-View-UI',
-  favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  title: 'React-Component-Library',
+  favicon:'https://gw.alipayobjects.com/zos/bmw-prod/d3e3eb39-1cd7-4aa5-827c-877deced6b7e/lalxt4g3_w256_h256.png',
+  logo:'https://gw.alipayobjects.com/zos/bmw-prod/d3e3eb39-1cd7-4aa5-827c-877deced6b7e/lalxt4g3_w256_h256.png',
   outputPath: 'docs-dist',
   mode: 'site',
   apiParser: {
