7caa7063e373934e28750906fd6618260703545b
李想同学 <135834691+username-boy@users.noreply.github.com>
2023-06-25 21:06:06 +0800

    react-component-library-view-initialization

---
+++ lx-React-View-UI-code/.umirc.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'React-View-UI',
+  favicon:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  outputPath: 'docs-dist',
+  mode: 'site',
+  apiParser: {
+    // 自定义属性过滤配置，也可以是一个函数，用法参考：https://github.com/styleguidist/react-docgen-typescript/#propfilter
+    propFilter: {
+      // 是否忽略从 node_modules 继承的属性，默认值为 false
+      skipNodeModules: true,
+      // 需要忽略的属性名列表，默认为空数组
+      skipPropsWithName: ['autoFocus', 'form', 'formAction', 'formEncType', 'title'],
+      // 是否忽略没有文档说明的属性，默认值为 false
+      skipPropsWithoutDoc: false,
+    },
+  },
+  history: {
+    type: 'hash',
+  },
+  navs: [
+    null,
+    {
+      title: 'GitHub',
+      path: 'https://github.com/username-boy/react-component-library-view',
+    },
+  ],
+});
