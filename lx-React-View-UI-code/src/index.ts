0d76d90469c7f3f48e2f18ec81169556648d5772
username_Lx <2308483480@qq.com>
2023-06-26 11:26:43 +0800

    feat(src/Patgination): 新增Patgination分页功能

--- lx-React-View-UI-code/src/index.ts
+++ lx-React-View-UI-code/src/index.ts
@@ -5,4 +5,5 @@ export { default as Select } from './Select';
 export { default as DatePicker } from './DatePicker';
 export { default as Menu } from './Menu';
 export { default as Layout } from './Layout';
+export { default as Pagination } from './Pagination';
 
