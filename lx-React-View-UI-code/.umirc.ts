a4881da22e79bf90997f988be287a71f316ccfe8
username_Lx <2308483480@qq.com>
2023-07-06 11:56:21 +0800

    修改了配置项

--- lx-React-View-UI-code/.umirc.ts
+++ lx-React-View-UI-code/.umirc.ts
@@ -6,6 +6,8 @@ export default defineConfig({
   logo: 'https://gw.alipayobjects.com/zos/bmw-prod/d3e3eb39-1cd7-4aa5-827c-877deced6b7e/lalxt4g3_w256_h256.png',
   outputPath: 'docs-dist',
   mode: 'site',
+  publicPath: '/react-component-library-view',
+  runtimePublicPath: true,
   apiParser: {
     // 自定义属性过滤配置，也可以是一个函数，用法参考：https://github.com/styleguidist/react-docgen-typescript/#propfilter
     propFilter: {
